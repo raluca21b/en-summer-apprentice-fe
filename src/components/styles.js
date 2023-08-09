d73e8be055e34c39ea536fbeaac0673dda185b37
Raluca Bulz <ralubulz@gmail.com>
2023-08-09 15:50:31 +0300

    Get events and Post Order done

---
+++ src/components/styles.js
@@ -0,0 +1,20 @@
+const bookOfStyles = {
+    purchase:[
+        'bg-white',
+        'px-4',
+        'py-4',
+        'gap-x-4',
+        'sm:border-b',
+        'sm:border-gray-200',
+        'flex',
+    ],
+    purchaseTitle: [
+        'text-lg',
+        'font-medium',
+        'text-orange-700',
+        'flex-1'
+    ],
+    purchaseQuantity:[
+        
+    ]
+}
